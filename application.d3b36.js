645adf01adf3c2167551713855cbb36bee35e441
t-nguyen <tunv@54vie.vn>
2026-03-16 20:17:28 +0700

    chore: english

--- application.d3b36.js
+++ application.d3b36.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.822f9.json';
+          this.settingsPath = 'src/settings.c32cb.json';
           this.showFPS = false;
         }
 
--- index.034ed.js
+++ index.034ed.js
@@ -1,4 +1,4 @@
-System.register(["./application.1f993.js"], function (_export, _context) {
+System.register(["./application.d3b36.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.1f993.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application1f993Js) {
-      Application = _application1f993Js.Application;
+    setters: [function (_applicationD3b36Js) {
+      Application = _applicationD3b36Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
