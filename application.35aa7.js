bfb56408c58e4e99a7a108c5d7f215103fe62691
t-nguyen <tunv@54vie.vn>
2026-03-20 02:59:55 +0700

    chore: fix bugs

--- application.35aa7.js
+++ application.35aa7.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.c32cb.json';
+          this.settingsPath = 'src/settings.a1d17.json';
           this.showFPS = false;
         }
 
--- index.35a8e.js
+++ index.35a8e.js
@@ -1,4 +1,4 @@
-System.register(["./application.d3b36.js"], function (_export, _context) {
+System.register(["./application.35aa7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.d3b36.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationD3b36Js) {
-      Application = _applicationD3b36Js.Application;
+    setters: [function (_application35aa7Js) {
+      Application = _application35aa7Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
