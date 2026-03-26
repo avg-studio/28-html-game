9f095c933a2ac4b29c97aadbde4a758626f89b8e
t-nguyen <tunv@54vie.vn>
2026-03-26 21:49:18 +0700

    chore: fix bugs

--- application.4b058.js
+++ application.4b058.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.798f2.json';
+          this.settingsPath = 'src/settings.fff5f.json';
           this.showFPS = false;
         }
 
