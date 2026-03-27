04e6018daa45cbb6bb07e084be3655e207c35385
t-nguyen <tunv@54vie.vn>
2026-03-28 01:05:54 +0700

    chore: fix bugs anim

--- application.8390a.js
+++ application.8390a.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.966e8.json';
+          this.settingsPath = 'src/settings.79776.json';
           this.showFPS = false;
         }
 
--- index.f8723.js
+++ index.f8723.js
@@ -1,4 +1,4 @@
-System.register(["./application.2c9a1.js"], function (_export, _context) {
+System.register(["./application.8390a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.2c9a1.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application2c9a1Js) {
-      Application = _application2c9a1Js.Application;
+    setters: [function (_application8390aJs) {
+      Application = _application8390aJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
